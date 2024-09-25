eb5c7e7f093f40097e4290ab6e6c1279558b950b
GabiFri <65342791+gabifri@users.noreply.github.com>
2024-09-25 10:47:45 +0200

    PTZ-V3
    control pano with analog joystick OK

--- PTZ-OS/PTZ-OS.ino
+++ PTZ-OS/PTZ-OS.ino
@@ -1,11 +1,16 @@
-  //PTZ - V2 : control pano with analog joystick OK
+  //PTZ - V3 : control pano with analog joystick OK
+  //Cobe by VLTV & GAB
+  //Dev by GALEX.CO by GALEXGROUP
+  //Projet Main by Léo TORRAS and CARL KB
   int speed=50; // Cette valeur détermine la vitesse du moteur, 1 = rapide, 100 = lent
   int pano = 12; // signal de l'axe X sur entrée A0
   int tilt = 14; // signal de l'axe Y sur entrée A1
-  int BJ = 17; // Bouton-poussoir en broche 7
+  int BJ = 27; // Bouton-poussoir en broche 7
   int step = 0;
   int PanoC1 = 16;
+  int TiltC1 = 18;
   int DirPanoC1 = 4;
+  int DirTiltC1 = 5;
 
   void setup() {
       pinMode(DirPanoC1, OUTPUT); // On initialise les broches D4 et D5 en sorties
@@ -21,6 +26,7 @@
   }
 
   void loop() {
+      //Initialisation Variable
       delay(1000);
       float X, Y;
       int B;
@@ -28,6 +34,7 @@
       Y = analogRead (tilt) * (5.0 / 1023.0);
       B = digitalRead(BJ);
 
+      //Indication Serial Print Verif Var
       //Serial.print("BJ =");
       //Serial.print (B);
       //Serial.print("\n");
@@ -38,7 +45,8 @@
       //Serial.print (Y, 4);
       //Serial.print ("V, ");
       
-
+    //Mouvement Pano C1
+      //Droite
     if(Y<=0){
         step = step+1;
         //Serial.println("Pano go right");
@@ -50,6 +58,7 @@
         delay(speed);
     }
 
+      //Gauche
     if(Y>=19){
         //Serial.print("Pano go left");
         //Serial.print("\n");
@@ -61,7 +70,32 @@
         delay(speed);
     }
 
-    if(digitalRead(BJ)==HIGH){
+    //Mouvement Tilt C1
+      //Haut
+    if(X<=0){
+        step = step+1;
+        //Serial.println("Pano go right");
+        //Serial.print("\n");
+        digitalWrite(DirTitlC1, HIGH);
+        digitalWrite(TiltC1, LOW);
+        delay(speed); // On envoie 200 impulsions pour faire tourner le moteur de 200 pas dans un sens
+        digitalWrite(TiltC1, HIGH); // La valeur de tempo determine le temps entre 2 impulsions en ms
+        delay(speed);
+    }
+
+      //Bas
+    if(X>=19){
+        //Serial.print("Pano go left");
+        //Serial.print("\n");
+        step = step-1;
+        digitalWrite(DirTitlC1, LOW);
+        digitalWrite(TitlC1, LOW);
+        delay(speed); // On envoie 200 impulsions pour faire tourner le moteur de 200 pas dans un sens
+        digitalWrite(TitlC1, HIGH); // La valeur de tempo determine le temps entre 2 impulsions en ms
+        delay(speed);
+    }
+
+    if(digitalRead(BJ)==LOW){
         //Serial.print("BJ Press");
         //Serial.print("\n");
         //Serial.print(step);
